5ca7d28d055619c6607e03c705faa5b1fac1c091
briansenas <briansena23@hotmail.com>
2022-11-03 14:39:27 +0100

    Error increase over time is fu*** up

--- accel.ino
+++ accel.ino
@@ -12,66 +12,56 @@ bool takeoff;
 unsigned int delvar;
 char flipside;
 
-void WiFiEvent(WiFiEvent_t event)
-{
-	switch (event)
-	{
+float pitch,roll,yaw;
+long gyro_x_cal, gyro_y_cal, gyro_z_cal;
+int gyro_x, gyro_y, gyro_z;
+
+void WiFiEvent(WiFiEvent_t event) {
+  switch (event) {
 #ifdef ESP8266
-	case WIFI_EVENT_STAMODE_GOT_IP:
+    case WIFI_EVENT_STAMODE_GOT_IP:
 #else
-	case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+    case ARDUINO_EVENT_WIFI_STA_GOT_IP:
 #endif
-		Serial.print("WiFi connected! IP address: ");
-		Serial.println(WiFi.localIP());
-		TelloCLI.begin();
-		// Turn off telemetry if not needed.
-		TelloCLI.setTelemetry(false);
-		Connected = true;
-    digitalWrite(18,HIGH);
-    digitalWrite(17,HIGH);
-    digitalWrite(16,LOW);
-		break;
+      Serial.print("WiFi connected! IP address: ");
+      Serial.println(WiFi.localIP());
+      TelloCLI.begin();
+      // Turn off telemetry if not needed.
+      TelloCLI.setTelemetry(false);
+      Connected = true;
+      break;
 #ifdef ESP8266
-	case WIFI_EVENT_STAMODE_DISCONNECTED:
+    case WIFI_EVENT_STAMODE_DISCONNECTED:
 #else
-	case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+    case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
 #endif
-		Serial.println("WiFi lost connection");
-		TelloCLI.end();
-		Connected = false;
-		break;
-	default:
-		break;
-	}
+      Serial.println("WiFi lost connection");
+      TelloCLI.end();
+      Connected = false;
+      break;
+    default:
+      break;
+  }
 }
 
-void setup()
-{
-  pinMode(18,OUTPUT);
-  pinMode(17,OUTPUT);
-  pinMode(16,OUTPUT);
-  
-  digitalWrite(18,HIGH);
-  digitalWrite(17,LOW);
-  digitalWrite(16,LOW);
-
-	Serial.begin(115200);
-	Serial.setTimeout(0);
-
-	delvar = 50;
-	Connected = false;
-	Command = false;
-  commandSent=false;
-	takeoff = false;
-	flipside = 'f'; //Front
-
+void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(0);
+  roll = 0;
+  pitch = 0;
+  yaw = 0;
+  delvar = 50;
+  Connected = false;
+  Command = false;
+  commandSent = false;
+  takeoff = false;
+  flipside = 'f';  //Front
 
   WiFi.mode(WIFI_STA);
   WiFi.onEvent(WiFiEvent);
 
   Wire.begin();
   Wire.setTimeOut(3000);
-
   delay(100);
   while (sensor.wakeup() == false) {
     Serial.print(millis());
@@ -91,19 +81,30 @@ void setup()
   sensor.gxe = 4.010;
   sensor.gye = 0.277;
   sensor.gze = -1.428;
+  Serial.println("INICIALIZANDO");
+
 }
 
-void loop()
-{
+void loop() {
   sensor.read();
 
-	if (!Connected)
-	{
+  float _pitch = sensor.getPitch();
+  float _roll = sensor.getRoll();
+  float _yaw = sensor.getYaw();
+
+
+  pitch = constrain(0.9*pitch+0.1*_pitch, -100, 100);
+  roll = constrain(0.9*roll + 0.1*_roll, -100, 100);
+  _yaw = int(_yaw)/20 * 20;
+  yaw = constrain(0.9*yaw+0.1*_yaw, -100, 100);
+  yaw = yaw * -1;
+
+  Serial.println("P: " + String(pitch) + " R: " + String(roll) + " Y: " + String(yaw));
+  if (!Connected) {
     String command = "TELLO-5ACC7D";
     char SSID[65];
     strcpy(SSID, command.c_str());
     WiFi.begin(SSID);
-    delay(1000);
   }else if(Connected && !Command){
     String command = "command";
     TelloCLI.write(command.c_str(), command.length());
@@ -111,15 +112,9 @@ void loop()
     command = "speed 10";
     TelloCLI.write(command.c_str(), command.length());
     delay(100);
-    Command = true; 
-    digitalWrite(18,LOW);
-    digitalWrite(17,HIGH);
-    digitalWrite(16,LOW);
-	}
-	else
-	{
-		if (touchRead(13) <= 30)
-		{
+    Command = true;
+  } else {
+    if (touchRead(13) <= 30) {
       String command = "rc 0 0 0 0";
       TelloCLI.write(command.c_str(), command.length());
       delay(30);
@@ -131,20 +126,11 @@ void loop()
         takeoff = true;
       }
       TelloCLI.write(command.c_str(), command.length());
-		}
-		else if (touchRead(33) <= 30 && takeoff)
-		{
-			String command = "flip ";
-			command += flipside;
-			TelloCLI.write(command.c_str(), command.length());
-		}
-		else if(!takeoff)
-		{
-			double eje_x = sensor.getAccelX()*-1;
-			double eje_y = sensor.getAccelY();
-
-      float pitch = 0; 
-      float roll = 0; 
+    } else if (touchRead(33) <= 30 && takeoff) {
+      String command = "flip ";
+      command += flipside;
+      TelloCLI.write(command.c_str(), command.length());
+    } else if (takeoff) {
       float throttle = 0;
 
       if (touchRead(32) <= 30) {
@@ -153,39 +139,24 @@ void loop()
       if (touchRead(4) <= 30) {
         throttle += 40;
       }
+      if (roll > 0) {
+        flipside = 'r';
+      } else {
+        flipside = 'l';
+      }
 
-			if (eje_x > 0.35 || eje_x < -0.35)
-			{
-				//Direccion x positiva: hacia la derecha, negativa: hacia la izquierda
-        pitch = eje_x * 60;
-				if(eje_x > 0){
-					flipside = 'r';
-				}
-				else
-				{
-					flipside = 'l';
-				}
-			}
-			if (eje_y > 0.35 || eje_y < -0.35)
-			{
-        roll = eje_y * 60; 
-				if(eje_y > 0){
-					flipside = 'f';
-				}
-				else
-				{
-					flipside = 'b';
-				}
-			}
-			String command = "rc " + String(roll) + " " + String(pitch) + " " + String(throttle) + " 0";
-      //Serial.println(command);
-			TelloCLI.write(command.c_str(), command.length());
-			commandSent = true;
-		}
-		if (commandSent)
-		{
-			commandSent = false;
-			delay(delvar);
-		}
-	}
+      if (pitch > 0) {
+        flipside = 'f';
+      } else {
+        flipside = 'b';
+      }
+      String command = "rc " + String(roll) + " " + String(pitch) + " " + String(throttle) + " " + String(yaw);
+      TelloCLI.write(command.c_str(), command.length());
+      commandSent = true;
+    }
+    if (commandSent) {
+      commandSent = false;
+      delay(delvar);
+    }
+  }
 }
